f233c43428f99ecf275aaf186086f7c7721e7f56
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-03-13 13:25:05 +0300

    updates the styling of navbar and homepage

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -52,7 +52,8 @@ const Home = () => {
                 <div>
                     <Container>
                         <div className="d-flex justify-content-center btn-div">
-                            <div className="btn-div-left">
+                            <div className="btn-div-left ">
+                                <img className="arrow-svg" src="/assets/undraw_fun-arrow.svg" alt="arrow-svg" />
                                 <Button className="main-btn get-started-btn">
                                     Get started
                                 </Button>
@@ -68,12 +69,25 @@ const Home = () => {
                 <div className="homepage-div homepage-div-bottom">
                     <Container>
                         <div className="d-flex">
-                            <div className="col-6 bottom-img-div d-flex flex-end">
-                                <img className="bottom-img" src="/assets/around_the_world.png" alt="around-the-world" />
+                            <div className="col-6 bottom-img-div d-flex justify-content-center">
+                                <img className="home-img bottom-img" src="/assets/around_the_world.png" alt="around-the-world" />
                             </div>
-                            <div className="col-6 bottom-header">
-                                fdssgs
+                            <div className="col-6 d-flex flex-column justify-content-center  bottom-header">
+                                <div className="d-flex bottom-header-1 mb-3 ">
+                                No lessons, no waiting. 
+                                </div>
+                                <div className="d-flex  bottom-header-2 justify-content-end">
+                                Find yourself a speaking opportunity
+                                </div>
+
+                                <div className="d-flex bottom-header-4 justify-content-end mb-3">
+                                at anytime, anywhere!
+                                </div>
+                            <div className="d-flex  bottom-header-2 justify-content-end">
+                            <Button className="discover-btn">DISCOVER</Button>
                             </div>
+                            </div>
+
                         </div>
                     </Container>
                 </div>
